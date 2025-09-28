5a3521667373ec740a28dcf04aa5bd7a32ac18f8
sneha112222189 <snehagangwarbhr@gmail.com>
2025-09-28 16:53:35 +0530

    renamed

--- linearSearch.java
+++ linearSearch.java
@@ -1,20 +1,26 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        int target = 2;
-        int ans = linearsearch(arr,target);
-        System.out.println(ans);
-    }   
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {12,352,76,837,88};
+        System.out.println("Enter the target element: ");
+        int target = sc.nextInt();
 
-    static int linearsearch(int[] arr, int target){
-        
-        if(arr.length==0) return -1;
-
-        for(int i=0;i<arr.length;i++){
-            int e = arr[i];
-            if(target==e) {
-                return arr[i];
+        if(linear(arr,target) != -1){
+            System.out.println("element is present in array");
         }
+        else{
+            System.out.println("element is not present");
+        }
+        
+    }
+
+    static int linear(int[] arr,int target){
+    if(arr.length == 0) return -1;
+
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]==target) return target;
     }
         return -1;
     }
