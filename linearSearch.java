4aaedd38e1849c2f3a3a7fea5b7fee111e394255
sneha112222189 <snehagangwarbhr@gmail.com>
2025-09-28 16:57:45 +0530

    done

--- linearSearch.java
+++ linearSearch.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try(Scanner sc = new Scanner(System.in);){
         int[] arr = {12,352,76,837,88};
         System.out.println("Enter the target element: ");
         int target = sc.nextInt();
@@ -15,6 +15,7 @@ class Main {
         }
         
     }
+}
 
     static int linear(int[] arr,int target){
     if(arr.length == 0) return -1;
