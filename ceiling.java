8214a40a04529342a327b1c56af158071a50d639
sneha112222189 <snehagangwarbhr@gmail.com>
2025-11-03 22:31:32 +0530

    save

---
+++ ceiling.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ceiling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr={1,12,43,54,99,100};
+        System.out.println("Enter the target element: ");
+        int target = sc.nextInt();
+        
+
+    }
+}
