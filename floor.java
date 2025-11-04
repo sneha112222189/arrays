c2492b7d00a73c2ef1709e198627d8688e35d62a
sneha112222189 <snehagangwarbhr@gmail.com>
2025-11-04 20:01:08 +0530

    save changes

--- floor.java
+++ floor.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
 class Main {
-    static int binarySearch(int[] arr,int low,int high,int target){
+
+    static int floor(int[] arr,int low,int high,int target){
             while(low<high){
             int mid = (low+(high-low))/2;
             if(arr[mid]==target)
                 return arr[mid];
             else if(arr[mid]<target){
-                return binarySearch(arr,mid+1,high,target);
+                return floor(arr,mid+1,high,target);
             }
             else{
-                return binarySearch(arr,low,mid,target);
+                return floor(arr,low,mid,target);
             }
         }
-        return arr[low];
+        return arr[high];
         }
     
 
@@ -22,6 +23,6 @@ class Main {
         int[] arr={10,20,30,40,50};
         System.out.println("enter the target element: ");
         int target = sc.nextInt();
-        System.out.println(binarySearch(arr,0,arr.length-1,target));
+        System.out.println(floor(arr,0,arr.length-1,target));
     }
 }
